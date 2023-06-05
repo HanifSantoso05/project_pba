4c40a735534bbe897e2103e80bee67bc8330c77e
Muhammad Hanif Santoso <84001908+HanifSantoso05@users.noreply.github.com>
2023-06-05 11:43:49 +0700

    Update pba_fix.py

--- pba_fix.py
+++ pba_fix.py
@@ -140,7 +140,7 @@ with st.container():
             lower_case_isi,clean_symbols,slang,stem = prep_input_data(word, slang_dict)
             
             # #Prediksi
-            v_data = tfidfvectorizer.transform([stem]).toarray()
+            v_data = loaded_data_tfid.transform([stem]).toarray()
             y_preds = clf.predict(v_data)
 
             st.subheader('Preprocessing')
